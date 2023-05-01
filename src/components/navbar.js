3d557c30db19449d014b4e5e91fd38d130c7f68f
SanC005 <82254558+ReiZero1349@users.noreply.github.com>
2023-05-02 01:39:41 +0530

    imporved navbar and bg

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import "./navbar.css";
+// import {GitHubIcon} from '@mui/icons-material/GitHub';
+import {FaGithub,FaLinkedin,} from "react-icons/fa";
+import { AiOutlineMail } from "react-icons/ai";
 const Navbar = () => {
     return(
     <>
@@ -10,15 +13,36 @@ const Navbar = () => {
                 </h2>
             </div>
             <div className="menu">
-                <ul>
+                <ul className='menu-ul'>
                     <li>
-                        <a href="#home">Home</a>
+                        <a href="#home">
+                        Home
+                        </a>
                     </li>
                     <li>
                         <a href="#about">About</a>
                     </li>
                     <li>
                         <a href="#contact">Contact</a>
+                    </li>
+                </ul>
+            </div>
+            <div className="social">
+                <ul className='social-ul'>
+                    <li>
+                        <a href="#github">
+                        <FaGithub />
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#linkedin">
+                        <FaLinkedin/>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#email">
+                            <AiOutlineMail/>
+                        </a>
                     </li>
                 </ul>
             </div>
