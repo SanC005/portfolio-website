c58972309b13a89b7c25be8543a3db28f5be9300
SanC005 <82254558+ReiZero1349@users.noreply.github.com>
2023-06-16 12:20:07 +0530

    updated home section with lottiefile

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -4,19 +4,28 @@ import About from "./about";
 import Type from "./type";
 import Project from "./project";
 import Contact from "./contact";
+import Lottie from 'lottie-react';
+import coding from "../assets/95348-coding-boy.json"
+<script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
 function home() {
+  const style = {
+    height: 300,
+  };
   return (
-    <section className="sec-home">
-      <div className="home-section" id="home">
+    <section className="sec-page">
+      <div className="home-section">
         <div className="home-content">
             <div className="home-header">
               <h1 className="heading">
                 Hi, I'm Sandeep!<br/>
-                I'm a Web Developer!<br/>
               </h1>
-              <div className="home-type"><Type /></div>
-              
+              <div className="home-type">
+                <Type /></div>
             </div>
+            <div className="lottie">
+              <Lottie animationData={coding} style={style}/>;
+            </div>
+            
                 
         </div>
       </div>
